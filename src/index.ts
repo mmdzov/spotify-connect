acf8dc6628f3aac2df584f14c2365968303fd6a9
mmdzov <mzov939@gmail.com>
2021-09-04 01:47:57 +0430

    Fixed some bugs

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// import SpotifyConnect from "./spotifyConnect";
+// import express from "express";
+// let spotify = new SpotifyConnect();
+
+// spotify.Config(
+//   "cb9db34d087c4f6dbcbc31fb5a7d1baf",
+//   "28671d0d5dcb4dd6bf63ba804a81677b",
+//   {
+//     port: 4000,
+//   },
+//   ["playlist-read-private"]
+// );
+
+// // spotify.spotifyAuthorize();
+// spotify.newtoken(
+//   "AQBn1-ejpSTFJycreEtGmn2fRWxPPMcour4ORG9QMnf8Rtaqxiw4NWHBtkieGn1EHpOSsZT19hW3ywr1ccCFboJO0aEFLrYfaZ0G8DOtwbMXkpYD0MDgLyKUXk1Hf4QBa6s",
+//   () => {}
+// );
+
+export * from "./spotifyConnect";
